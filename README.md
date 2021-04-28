5652d0f2420751bd6b403e94158816f0bb28948f
bagasapk <bagasapk>
2021-04-28 16:31:14 +0700

    MB add Login,Register,Roles

---
+++ README.md
@@ -0,0 +1,12 @@
+Hello Kema MIPA Unpad!</br>
+This code is for MIPA Bersatu 2021 Event's website. Server-side Programs!<br>
+<br>
+Developed by<br>
+Giovanni Alfadiansyah<br>
+1408101700XX<br>
+<br>
+Anugerah Prima Bagaskara<br>
+140810180006<br>
+<br>
+Computer Science<br>
+Padjadjaran University
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        if ($request->user()->hasRole('user')) {
+            return redirect('user');
+        }
+
+        if ($request->user()->hasRole('admin')){
+            return redirect('admin');
+        }
+    }
+}
